c8c8c280b6218ed37036b84d0d5981cf93fe4882
Mojmir Durik <durik@run.sk>
2026-07-09 16:56:01 +0200

    Devcontainers - creation of scratchpad file tmp/tmp.txt (if not exists yet)  on container startup

--- odoo-19/.devcontainer/post-start.sh
+++ odoo-19/.devcontainer/post-start.sh
@@ -11,3 +11,12 @@ ln -s /var/log/odoo /mnt/tmp/log
 
 # clear log file
 truncate -s 0 /var/log/odoo/odoo.log 2>/dev/null || true
+
+# create scratchpad file if it does not exist
+echo "" && echo "Creating scratchpad file..."
+SCRATCHPAD_DIR="$(dirname "${BASH_SOURCE[0]}")/../tmp"
+SCRATCHPAD_FILE="${SCRATCHPAD_DIR}/tmp.txt"
+if [ ! -f "${SCRATCHPAD_FILE}" ]; then
+    mkdir -p "${SCRATCHPAD_DIR}"
+    echo "This is your scratchpad..." > "${SCRATCHPAD_FILE}"
+fi
