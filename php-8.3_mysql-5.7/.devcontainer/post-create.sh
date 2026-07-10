1f26560cb31ad4f6639f95e0352ad20057762a41
Mojmir Durik <durik@run.sk>
2026-07-10 15:10:03 +0200

    Devcontainers > post-create.sh - conditional launching of git tweaks

--- php-8.3_mysql-5.7/.devcontainer/post-create.sh
+++ php-8.3_mysql-5.7/.devcontainer/post-create.sh
@@ -10,14 +10,17 @@ sudo pip install -r ./.devcontainer/requirements.txt
 echo "" && echo "Installing runtools..."
 pipx install "runtools @ git+https://git@github.com/RunDevelopmentSk/runtools.git@main"
 
-# git: add safe.directory exception for workspace folder (* = for any folder in fact)
-# because the project folder is bind-mounted from the host where it may be owned by
-# a different UID than the container user. This is mostly case on Windows.
-# Use append (>>) to avoid EBUSY caused by atomic rename on bind-mounted file.
-if ! grep -qE '^[[:space:]]*directory[[:space:]]*=[[:space:]]*\*[[:space:]]*$' "$HOME/.gitconfig" 2>/dev/null; then
-    printf '\n[safe]\n\tdirectory = *\n' >> "$HOME/.gitconfig"
-fi
+if command -v git >/dev/null 2>&1; then
+    # git: add safe.directory exception for workspace folder (* = for any folder in fact)
+    # because the project folder is bind-mounted from the host where it may be owned by
+    # a different UID than the container user. This is mostly case on Windows.
+    # Use append (>>) to avoid EBUSY caused by atomic rename on bind-mounted file.
+    if ! grep -qE '^[[:space:]]*directory[[:space:]]*=[[:space:]]*\*[[:space:]]*$' "$HOME/.gitconfig" 2>/dev/null; then
+        printf '\n[safe]\n\tdirectory = *\n' >> "$HOME/.gitconfig"
+    fi
 
-# git: install pre-commit hooks
-echo "" && echo "Installing precommit..."
-pre-commit install
+    # git: install pre-commit hooks
+    echo "" && echo "Installing precommit..."
+    sudo pip install pre-commit
+    pre-commit install
+fi
